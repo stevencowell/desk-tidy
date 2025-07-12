d399f18a0b943b8c238b16854f088ae7cc202c87
stevencowell <steven.cowell@det.nsw.edu.au>
2025-07-12 12:49:14 +1000

    Split theory sections into individual week files

--- loadSections.js
+++ loadSections.js
@@ -8,6 +8,12 @@ const sections = [
   'advanced-activities'
 ];
 
+const weekCounts = {
+  'main-theory': 10,
+  'support-activities': 10,
+  'advanced-activities': 10
+};
+
 function loadSections() {
   return Promise.all(sections.map(id => {
     return fetch('sections/' + id + '.html')
@@ -19,9 +25,31 @@ function loadSections() {
   }));
 }
 
+function loadWeeks() {
+  const promises = [];
+  for (const [section, count] of Object.entries(weekCounts)) {
+    const container = document.getElementById(section + '-weeks');
+    if (!container) continue;
+    for (let i = 1; i <= count; i++) {
+      promises.push(
+        fetch(`sections/${section}/week${i}.html`)
+          .then(resp => resp.text())
+          .then(html => {
+            const div = document.createElement('div');
+            div.innerHTML = html;
+            container.appendChild(div);
+          })
+      );
+    }
+  }
+  return Promise.all(promises);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  loadSections().then(() => {
-    if (typeof initPage === 'function') initPage();
-    if (typeof initQuizFeatures === 'function') initQuizFeatures();
-  });
+  loadSections()
+    .then(loadWeeks)
+    .then(() => {
+      if (typeof initPage === 'function') initPage();
+      if (typeof initQuizFeatures === 'function') initQuizFeatures();
+    });
 });
